127e843c6548c711332fbb09b1f4983264d9f973
Brison Kinsella <KTBK53@GMAIL.COM>
2026-09-14 17:10:26 -0400

    Created all Arrays required for the application. Names, Prices, Items ordered, Item quanities, Item prices, and the amout of items ordered.

--- PizzaApplication/Program.cs
+++ PizzaApplication/Program.cs
@@ -1,76 +1,18 @@
-﻿
-
-public class Menu
+﻿public class main
 {
-    string[] Pizza = { "Margherita", "Pepperoni", "Hawaiian", "Veggie", "BBQ Chicken" };
+    string[] Pizzas = { "Margherita", "Pepperoni", "Hawaiian", "Veggie", "BBQ Chicken" };
     string[] Drinks = { "Coke", "Pepsi", "Sprite", "Fanta", "Water" };
-    public string[] GetPizzaMenu()
-    {
-        return Pizza;
-    }
-    public string[] GetDrinksMenu()
-    {
-        return Drinks;
-    }
-}
-public class PizzaOrder
-{
-    public string PizzaType { get; set; }
-    public int Quantity { get; set; }
-    public string DrinkType { get; set; }
-    public int DrinkQuantity { get; set; }
-    public PizzaOrder(string pizzaType, int quantity, string drinkType, int drinkQuantity)
-    {
-        PizzaType = pizzaType;
-        Quantity = quantity;
-        DrinkType = drinkType;
-        DrinkQuantity = drinkQuantity;
-    }
-}
+    double[] PizzaPrices = { 8.99, 9.99, 10.99, 9.49, 11.49 };
+    double[] DrinkPrices = { 1.99, 1.99, 1.99, 1.99, 0.99 };
+    string[] ItemsOrdered = new string[10];
+    int[] ItemQuantities = new int[10];
+    double[] ItemPrices = new double[10];
+    int ItemCount = 0;
 
-public class PizzaAndDrinkPrice
-{
-    public decimal CalculatePrice(PizzaOrder order)
-    {
-        decimal pizzaPrice = 0;
-        decimal drinkPrice = 0;
-        switch (order.PizzaType)
-        {
-            case "Margherita":
-                pizzaPrice = 8.99m;
-                break;
-            case "Pepperoni":
-                pizzaPrice = 9.99m;
-                break;
-            case "Hawaiian":
-                pizzaPrice = 10.99m;
-                break;
-            case "Veggie":
-                pizzaPrice = 9.49m;
-                break;
-            case "BBQ Chicken":
-                pizzaPrice = 11.49m;
-                break;
-            default:
-                throw new ArgumentException("Invalid pizza type");
-        }
-        switch (order.DrinkType)
-        {
-            case "Coke":
-            case "Pepsi":
-            case "Sprite":
-            case "Fanta":
-                drinkPrice = 1.99m;
-                break;
-            case "Water":
-                drinkPrice = 0.99m;
-                break;
-            default:
-                throw new ArgumentException("Invalid drink type");
-        }
-        return (pizzaPrice * order.Quantity) + (drinkPrice * order.DrinkQuantity);
-    }
+
+
 }
+    
 
 
 
