1baf4ea17669fad12ad63407e9afbcb0d0671040
Jason Batts <jbatts29@gmail.com>
2026-09-16 16:51:51 -0400

    Added Main Menu, Pizza Ordering, Drink Ordering, and an dumb ASCII Title for fun.

--- PizzaApplication/Program.cs
+++ PizzaApplication/Program.cs
@@ -12,6 +12,197 @@ public class main
     int ItemCount = 0;
 
 
+    // Program starts here - Jason
+    static void Main(string[] args)
+    {
+        main app = new main();
+        app.MainMenu();
+    }
+
+    // Main Menu - Jason
+    void MainMenu()
+    {
+        bool running = true;
+
+        while(running)
+        {
+            Console.Clear();
+            TitleAscii();
+            Console.WriteLine("\n===Main Menu===\n");
+            Console.WriteLine("1. Order Pizza");
+            Console.WriteLine("2. Order Drinks");
+            Console.WriteLine("3. View Order");
+            Console.WriteLine("4. Checkout");
+            Console.WriteLine("5. Exit\n");
+
+            Console.Write("Please enter your choice: ");
+
+            string input = Console.ReadLine();
+            int choice;
+
+            if (int.TryParse(input, out choice) && choice >= 1 && choice <= 5)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        OrderPizza();
+                        break;
+
+                    case 2:
+                        OrderDrinks();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        ViewOrder();
+                        Console.ReadLine();
+                        break;
+
+                    case 4:
+                        Console.Clear();
+                        Checkout();
+                        Console.ReadLine();
+                        break;
+
+                    case 5:
+                        running = false;
+                        Console.Clear();
+                        Console.WriteLine("===Exit Screen===\n");
+                        Console.WriteLine("Thank you for using the PizzaApp!\n");
+                        Console.WriteLine("Have a nice day!");
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("\nSorry, please enter a number 1 through 5 for your choice.");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+    //Order Pizza - Jason
+    void OrderPizza()
+    {
+        //Loops to check user Pizza input
+        while (true)
+        {
+            //Prints Menu
+            Console.Clear();
+            Console.WriteLine("\n===Order Pizza Menu===\n");
+
+            for (int i = 0; i < Pizzas.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Pizzas[i]} - {PizzaPrices[i]:C}");
+            }
+            
+            Console.Write("\nPlease select a pizza: ");
+
+            string input = Console.ReadLine();
+            int pizzaChoice;
+
+            //Pizza Input Check
+            if (int.TryParse(input, out pizzaChoice) && pizzaChoice >= 1 && pizzaChoice <= Pizzas.Length)
+            { 
+                Console.WriteLine($"You selected: {Pizzas[pizzaChoice-1]} - {PizzaPrices[pizzaChoice-1]:C}");
+                int quantity;
+
+                //Loops to check user quantity input
+                while (true)
+                {
+                    Console.Write("\nPlease enter a quantity: ");
+                    string quantityInput = Console.ReadLine();
+
+                    //Quantiy Input Check
+                    if (int.TryParse(quantityInput, out quantity) && quantity > 0)
+                    {
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Invalid quantity. Please enter a number greater than zero.");
+                }
+
+                AddToOrder(Pizzas[pizzaChoice-1], quantity, PizzaPrices[pizzaChoice-1]);
+                Console.ReadLine();
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, please enter a number from 1 to {PizzaPrices.Length}.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+
+    //Order Drinks - Jason
+    void OrderDrinks()
+    {
+        //Loops to check Drinks input
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("\n===Order Drinks Menu===\n");
+
+            for (int i = 0; i < Drinks.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Drinks[i]} - {DrinkPrices[i]:C}");
+            }
+
+            Console.Write("\nPlease select a drink: ");
+
+            string input = Console.ReadLine();
+            int drinkChoice;
+
+            //Drinks input check
+            if (int.TryParse(input, out drinkChoice) &&
+                drinkChoice >= 1 &&
+                drinkChoice <= Drinks.Length)
+            {
+                Console.WriteLine(
+                    $"You selected: {Drinks[drinkChoice - 1]} - {DrinkPrices[drinkChoice - 1]:C}"
+                );
+
+                int quantity;
+
+                //Loops to check quantity input
+                while (true)
+                {
+                    Console.Write("\nPlease enter a quantity: ");
+                    string quantityInput = Console.ReadLine();
+
+                    //Quantity input check
+                    if (int.TryParse(quantityInput, out quantity) &&
+                        quantity > 0)
+                    {
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Invalid quantity. Please enter a number greater than zero.");
+                }
+
+                AddToOrder(
+                    Drinks[drinkChoice - 1],
+                    quantity,
+                    DrinkPrices[drinkChoice - 1]
+                );
+
+                Console.ReadLine();
+                break;
+            }
+            else
+            {
+                Console.WriteLine(
+                    $"Sorry, please enter a number from 1 to {Drinks.Length}."
+                );
+
+                Console.ReadLine();
+            }
+        }
+
+    }
     /* Add to Order */
 
     private void AddToOrder(string name, int quantity, double price) 
@@ -109,7 +300,20 @@ public class main
     Console.WriteLine($"Total: ${CalcTotal():F2}");
     Console.WriteLine("Thank you for your order!");
     }
-    }
+    
+    //Just a fun stupid title I made for the app main screen - Jason
+    void TitleAscii()
+    {
+        Console.WriteLine();
+        Console.WriteLine("       ^         || =                                         //      ");
+        Console.WriteLine("      / \\        ||   =                                      // \\\\        ");
+        Console.WriteLine("     /   \\       ||    ||  O   =====  =====     //          //   \\\\     ||==   ||==  ");
+        Console.WriteLine("    / O   \\      ||   =           //     //    // \\\\       //     \\\\    ||  || ||  ||");
+        Console.WriteLine("   /     O \\     || =      ||    //     //    //   \\\\     // ===== \\\\   ||==   ||== ");
+        Console.WriteLine("  /  O      \\    ||        ||   //     //    // === \\\\   //         \\\\  ||     ||   ");
+        Console.WriteLine(" ###    O   ###  ||        ||  =====  ===== //       \\\\ //           \\\\ ||     || ");
+        Console.WriteLine("   ##########");
+    }    
 }
     
 
